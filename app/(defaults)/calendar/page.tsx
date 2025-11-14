06dab53da86a9597687612451441168a28ecb358
Rifayet Uddin Ahmed <rifayet@Rifayets-MacBook-Air.local>
2025-11-14 17:08:51 +0800

    update header and sidebar

---
+++ app/(defaults)/calendar/page.tsx
@@ -0,0 +1,8 @@
+import ComponentsAppsCalendar from '@/components/apps/calendar/component-apps-calendar';
+import React from 'react';
+
+const Calendar = () => {
+    return <ComponentsAppsCalendar />;
+};
+
+export default Calendar;
--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -143,12 +143,7 @@ const Sidebar = () => {
                                 </AnimateHeight>
                             </li>
 
-                            <h2 className="-mx-4 mb-1 flex items-center bg-white-light/30 px-7 py-3 font-extrabold uppercase dark:bg-dark dark:bg-opacity-[0.08]">
-                                <IconMinus className="hidden h-5 w-4 flex-none" />
-                                <span>{t('HR')}</span>
-                            </h2>
-
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <ul>
                                     <li className="nav-item">
                                         <Link href="/staff/staff-list" className="group">
@@ -729,7 +724,7 @@ const Sidebar = () => {
                                         <span className="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{t('documentation')}</span>
                                     </div>
                                 </Link>
-                            </li>
+                            </li> */}
                         </ul>
                     </PerfectScrollbar>
                 </div>
