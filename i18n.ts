749cfb649770ce4391bb2c52acbe137843154728
Rifayet Uddin Ahmed <rifayet@Rifayets-MacBook-Air.local>
2025-11-14 17:33:38 +0800

    lang edit

--- i18n.ts
+++ i18n.ts
@@ -1,22 +1,67 @@
+// const cookieObj = typeof window === 'undefined' ? require('next/headers') : require('universal-cookie');
+
+// import en from './public/locales/en.json';
+// import ae from './public/locales/ae.json';
+// import da from './public/locales/da.json';
+// import de from './public/locales/de.json';
+// import el from './public/locales/el.json';
+// import es from './public/locales/es.json';
+// import fr from './public/locales/fr.json';
+// import hu from './public/locales/hu.json';
+// import it from './public/locales/it.json';
+// import ja from './public/locales/ja.json';
+// import pl from './public/locales/pl.json';
+// import pt from './public/locales/pt.json';
+// import ru from './public/locales/ru.json';
+// import sv from './public/locales/sv.json';
+// import tr from './public/locales/tr.json';
+// import zh from './public/locales/zh.json';
+// const langObj: any = { en, ae, da, de, el, es, fr, hu, it, ja, pl, pt, ru, sv, tr, zh };
+
+// const getLang = () => {
+//     let lang = null;
+//     if (typeof window !== 'undefined') {
+//         const cookies = new cookieObj(null, { path: '/' });
+//         lang = cookies.get('i18nextLng');
+//     } else {
+//         const cookies = cookieObj.cookies();
+//         lang = cookies.get('i18nextLng')?.value;
+//     }
+//     return lang;
+// };
+
+// export const getTranslation = () => {
+//     const lang = getLang();
+//     const data: any = langObj[lang || 'en'];
+
+//     const t = (key: string) => {
+//         return data[key] ? data[key] : key;
+//     };
+
+//     const initLocale = (themeLocale: string) => {
+//         const lang = getLang();
+//         i18n.changeLanguage(lang || themeLocale);
+//     };
+
+//     const i18n = {
+//         language: lang,
+//         changeLanguage: (lang: string) => {
+//             const cookies = new cookieObj(null, { path: '/' });
+//             cookies.set('i18nextLng', lang);
+//         },
+//     };
+
+//     return { t, i18n, initLocale };
+// };
+
 const cookieObj = typeof window === 'undefined' ? require('next/headers') : require('universal-cookie');
 
 import en from './public/locales/en.json';
-import ae from './public/locales/ae.json';
-import da from './public/locales/da.json';
-import de from './public/locales/de.json';
-import el from './public/locales/el.json';
-import es from './public/locales/es.json';
-import fr from './public/locales/fr.json';
-import hu from './public/locales/hu.json';
-import it from './public/locales/it.json';
-import ja from './public/locales/ja.json';
-import pl from './public/locales/pl.json';
-import pt from './public/locales/pt.json';
-import ru from './public/locales/ru.json';
-import sv from './public/locales/sv.json';
-import tr from './public/locales/tr.json';
-import zh from './public/locales/zh.json';
-const langObj: any = { en, ae, da, de, el, es, fr, hu, it, ja, pl, pt, ru, sv, tr, zh };
+import my from './public/locales/my.json';
+import id from './public/locales/id.json';
+import bd from './public/locales/bd.json';
+
+const langObj: any = { en, my, id, bd };
 
 const getLang = () => {
     let lang = null;
--- ni18n.config.ts.js
+++ ni18n.config.ts.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const supportedLngs = ['da', 'de', 'el', 'en', 'es', 'fr', 'hu', 'it', 'ja', 'pl', 'pt', 'ru', 'sv', 'tr', 'zh', 'ae'];
+const supportedLngs = ['en', 'my', 'id', 'bd'];
 import themeConfig from 'theme.config';
 export const ni18nConfig = {
     fallbackLng: [themeConfig.locale || 'en'],
