28e97af0faf8d79dc86d203d5dd975ba12efb8b7
Rifayet Uddin Ahmed <rifayet@Rifayets-MacBook-Air.local>
2025-11-26 10:19:39 +0800

    updated config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 // next.config.js
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
     reactStrictMode: false,
     eslint: { ignoreDuringBuilds: true },
     output: 'standalone',
-
-    basePath: '',
-    assetPrefix: '',
 
     images: {
         domains: ['devsec.awfatech.com'],
